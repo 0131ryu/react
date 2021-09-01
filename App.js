eaae932273a4e9da3f4b3bf7f8a19ecd4eaf02fa
0131ryu <89256977+0131ryu@users.noreply.github.com>
2021-09-01 23:31:38 +0900

    0901 쇼핑몰 만들기  복습 1
    현재 props를 통한 이미지&세부정보 데이터 바인딩, 이미지가 동일하게 적용됨(수정 필요)

--- App.js
+++ App.js
@@ -1,54 +1,59 @@
-import React from 'react';
-import { Navbar, Container, Nav, Jumbotron, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
 import './App.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import samsung1 from './images/samsung1.jpg';
+import samsung2 from './images/samsung2.jpg';
+import samsung3 from './images/samsung3.jpg';
+import samsung4 from './images/samsung4.jpg';
+import apple1 from './images/apple1.jpg';
+import apple2 from './images/apple2.jpg';
+import apple3 from './images/apple3.jpeg';
+
+import SItem from './SItem.js';
 
 function App() {
+
+  let [SItems, SItemsChange] = useState(SItem);
+
   return (
     <div className="App">
-       <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-
-  <Jumbotron className="background">
-  <h1>20% Season Off</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-    <Button variant="primary">Learn more</Button>
-  </p>
-</Jumbotron>
+      <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Electronic Mall</Navbar.Brand>
+    <Nav.Link >Introduce</Nav.Link>
+        <Nav.Link >Goods</Nav.Link>
+        <Nav.Link >location</Nav.Link>
+        <Nav.Link >inquiry</Nav.Link>
+  </Container>
+</Navbar>
+  
     
+  <div className="ad">
+      <h1 className="adTitle">[ 오픈 한정 행사 : 최대 30% 세일 ] </h1>
+  </div>
 
-    <div className="container">
-      <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
+  <div className="container">
+        <div className="row">
+        { 
+            SItems.map((a,i)=>{
+             return <SList SItems={SItems[i]} />
+            })
+        }
+        </div>
       </div>
-    </div>
+
     </div>
   );
+}
+
+function SList(props) {
+  return (
+    <div className="col-md-4">
+            <img src={samsung1} width="100%" className="firstImg"/>
+            <h4>{ props.SItems.title }</h4>
+            <p>{ props.SItems.content } & { props.SItems.price } </p>
+          </div>
+  )
 }
 
 export default App;
---
+++ SItem.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      id : 0,
+      title : "Galaxy Watch4 | Watch4 Classic",
+      content : "Made by Samsung",
+      price : 399000
+    },
+  
+    {
+      id : 1,
+      title : "Galaxy Z Flip3",
+      content : "Made by Samsung",
+      price : 1254000
+    },
+  
+    {
+      id : 2,
+      title : "Galaxy Z Fold 3",
+      content : "Made by Samsung",
+      price : 1998700
+    }
+  ] 
