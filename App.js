69526eb669f7d9cbe9c09c78fb0d5f6a8c894332
0131ryu <89256977+0131ryu@users.noreply.github.com>
2021-09-02 22:13:40 +0900

    0902 쇼핑몰 만들기2
    알림창 여부, scss 사용하기, hook 사용하기

--- App.js
+++ App.js
@@ -1,59 +1,84 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import samsung1 from './images/samsung1.jpg';
-import samsung2 from './images/samsung2.jpg';
-import samsung3 from './images/samsung3.jpg';
-import samsung4 from './images/samsung4.jpg';
-import apple1 from './images/apple1.jpg';
-import apple2 from './images/apple2.jpg';
-import apple3 from './images/apple3.jpeg';
+/*eslint-disable */
 
-import SItem from './SItem.js';
+import React, {useState} from 'react';
+import { Navbar, Container, Nav, Jumbotron, Button } from 'react-bootstrap';
+import './App.css';
+// import name from './data.js';
+//import { name, name2 } from './data.js';
+import Data from './data.js';
+import Detail from './Detail.js';
+
+
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
-  let [SItems, SItemsChange] = useState(SItem);
+  let [shoes, shoes변경] = useState(Data);
 
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Electronic Mall</Navbar.Brand>
-    <Nav.Link >Introduce</Nav.Link>
-        <Nav.Link >Goods</Nav.Link>
-        <Nav.Link >location</Nav.Link>
-        <Nav.Link >inquiry</Nav.Link>
-  </Container>
-</Navbar>
-  
-    
-  <div className="ad">
-      <h1 className="adTitle">[ 오픈 한정 행사 : 최대 30% 세일 ] </h1>
-  </div>
+       <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
+      <Nav.Link >Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
 
-  <div className="container">
-        <div className="row">
-        { 
-            SItems.map((a,i)=>{
-             return <SList SItems={SItems[i]} />
-            })
+ 
+  <Switch>
+    <Route exact path="/">
+    <Jumbotron className="background">
+  <h1>20% Season Off</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
+
+
+<div className="container">
+      <div className="row">
+        {
+          shoes.map((a, i) => { //a : shoes안에 들어간 하나하나의 데이터
+            return <Card shoes={shoes[i]} i={i} key={i}/>
+          })
         }
-        </div>
       </div>
+    </div>
+    </Route>
 
+    <Route path="/detail/:id"> 
+        <Detail shoes={shoes}/>
+    </Route>
+
+    {/* <Route path="/어쩌구" component={Modal}></Route> */}
+
+    <Route path="/:id"> {/* /모든문자라는 경로 의미 */}
+        <div> 아무거나 적었을 때 이걸 보여줌 </div>
+    </Route>
+
+    </Switch>
     </div>
   );
 }
 
-function SList(props) {
+function Card(props) {
   return (
-    <div className="col-md-4">
-            <img src={samsung1} width="100%" className="firstImg"/>
-            <h4>{ props.SItems.title }</h4>
-            <p>{ props.SItems.content } & { props.SItems.price } </p>
-          </div>
+    <div>
+       <img src={ "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg" } width="100%"/>
+       <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.content} <br/> { props.shoes.price }</p>
+    </div>
   )
 }
+
+
 
 export default App;
